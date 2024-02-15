836a658945bd4f5a7006471d67ee4bcaa7577066
Lnida <91710828+d4n1938@users.noreply.github.com>
2024-02-15 18:56:12 +0900

    change:create pages

---
+++ lib/pages/text_only.dart
@@ -0,0 +1,150 @@
+// ------------------------------ Text Only ------------------------------
+
+import 'package:flutter/material.dart';
+import 'package:google_gemini/google_gemini.dart';
+import 'package:test_bard/main.dart';
+
+class TextOnly extends StatefulWidget {
+  const TextOnly({
+    super.key,
+  });
+
+  @override
+  State<TextOnly> createState() => _TextOnlyState();
+}
+
+class _TextOnlyState extends State<TextOnly> {
+  bool loading = false;
+  List textChat = [];
+  List textWithImageChat = [];
+  int speakingStatus = -1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+  // Create Gemini Instance
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+
+  // Text only input
+  void fromText({required String query}) {
+    setState(() {
+      loading = true;
+      textChat.add({
+        "role": "User",
+        "text": query,
+      });
+      _textController.clear();
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromText(query).then((value) {
+      setState(() {
+        loading = false;
+        textChat.add({
+          "role": "Gemini",
+          "text": value.text,
+        });
+        tts.setLanguage('ja-JP'); // 読み上げる言語を日本語に設定
+        tts.setSpeechRate(0.5);
+        tts.speak(value.text); // 読み上げる
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textChat.add({
+          "role": "Gemini",
+          "text": error.toString(),
+        });
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd() {
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            controller: _controller,
+            itemCount: textChat.length,
+            padding: const EdgeInsets.only(bottom: 20),
+            itemBuilder: (context, index) {
+              return ListTile(
+                isThreeLine: true,
+                leading: CircleAvatar(
+                  child: Text(textChat[index]["role"].substring(0, 1)),
+                ),
+                title: Row(
+                  children: [
+                    Text(textChat[index]["role"]),
+                    const SizedBox(width: 10),
+                    textChat[index]["role"] == "Gemini"
+                        ? IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              speakingStatus != index
+                                  ? Icons.play_arrow
+                                  : Icons.stop,
+                            ))
+                        : const SizedBox()
+                  ],
+                ),
+                subtitle: Text(textChat[index]["text"]),
+              );
+            },
+          ),
+        ),
+        Container(
+          alignment: Alignment.bottomRight,
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    hintText: "Type a message",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none),
+                    fillColor: Colors.transparent,
+                  ),
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                ),
+              ),
+              IconButton(
+                icon: loading
+                    ? const CircularProgressIndicator()
+                    : const Icon(Icons.send),
+                onPressed: () {
+                  fromText(query: _textController.text);
+                },
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
